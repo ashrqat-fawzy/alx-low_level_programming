8bd6ce0d9618514120068caef038cb71a48e6c3d
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-15 15:29:12 -0700

    Initial commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the sizes of various data types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1.q.59);
+	return (1);
+}
