fb6179a2b0919c6692015f25fc2ee730eb544bf3
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-17 12:46:23 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A C program that prints a digits
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d);
+		d++;
+	}
+	return (0);
+}
