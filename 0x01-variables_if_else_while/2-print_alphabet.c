23c93a00422402af25a6b33d4483a3c00edcd8b2
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-16 19:49:43 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'c';
+
+	putchar(c);
+	putchar(c);
+
+	return (0);
+}
