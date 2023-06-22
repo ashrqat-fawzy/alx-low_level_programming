f3da3df094ba65ce32ed4e0819f464bab1ca8a5b
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-22 15:26:27 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+*Description: print the alphabet - unilizes on the _putchar function to print
+* the he alphabet, in lowercase
+*
+* Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = a; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
