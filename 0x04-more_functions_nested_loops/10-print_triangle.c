68fe3ed1281fbc4d45a215a949deaf22d7ca933c
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-24 09:31:15 -0700

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,22 +5,19 @@
  * @size: numbers of lines.
  * Return: no return.
 */
+
 void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	for (i = 0; i < size; i++)
 	{
-	_putchar('\n');
-	return;
-	}
-
-	for (i = 1; i <= size; i++)
-	{
-	for (j = 1; j <= i; j++)
-	{
-	_putchar('#');
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar('#');
+		if (i < (size - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
-	}
 }
