dfaef6bae9e2abd61da0ba58e55fc2cde89eabc1
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-27 12:25:52 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+*/
+
+void print_rev(char *s)
+{
+	while (*s != '\n')
+	{
+		putchar(*S + '0');
+		s--;
+	}
+	putchar('\n');
+}
