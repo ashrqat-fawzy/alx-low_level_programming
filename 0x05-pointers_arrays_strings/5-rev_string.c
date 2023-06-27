c2e9beba019a16ceb3ec26c92160cb023049827c
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-06-27 12:41:51 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+*/
+
+void rev_string(char *s)
+{
+	int l = 0;
+	int i;
+	int temp;
+
+	while (s[l])
+		l++;
+	for (i = 0; i < l; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = temp;
+	}
+
