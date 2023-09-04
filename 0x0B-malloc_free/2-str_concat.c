9f3f58949bdd4772762c679de7a76282867ca906
ashrqat.fawzy@gmail.com <ashrqat.fawzy@gmail.com>
2023-09-05 00:14:45 +0300

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,6 @@ char *str_concat(char *s1, char *s2)
 {
 	char *str;
 	int i, j, k, limit;
-	
 	if (s1 == NULL)
 		s1 =  "";
 	if (s2 == NULL)
@@ -21,8 +20,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
-		;
-	
+		;	
 	str = malloc(sizeof(char) * (i + j + 1));
 
 	if (str == NULL)
