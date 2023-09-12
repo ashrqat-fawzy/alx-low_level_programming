d8161330d5ba1d6469d297cf97e0fb92ab8789c5
ashrqat.fawzy@gmail.com <ashrqat.fawzy@gmail.com>
2023-09-12 03:02:29 +0300

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,6 @@
 #define _DOG_H_
 
 /**
- * dog_t - Typedef for struct dog
-*/
-
-typedef struct dog dog_t;
-
-/**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
  * @age: age of the dog
@@ -24,10 +18,6 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
 
