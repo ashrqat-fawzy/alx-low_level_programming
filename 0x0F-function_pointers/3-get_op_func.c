69ed91dcbfe10c5a6b235e59ae6120a3f1d8c556
ashrqat.fawzy@gmail.com <ashrqat.fawzy@gmail.com>
2023-09-14 03:27:14 +0300

    a

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,95 +1,30 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdio.h>
+#include <string.h>
 
 /**
- * op_add - function that sum of a and b
+ * get_op_func - function that selects the correct function operator
  *
- * @a: parameter is integer
+ * @s: the operator passed as argument to the program
  *
- * @b: parameter is integer
- *
- * Return:  returns the sum of a and b
+ * Return: returns pointer to the function
 */
 
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-return (a + b);
+op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+int i = 0;
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+return (ops[i].f);
 }
 
-/**
- * op_sub - function that difference of a and b
- *
- * @a: parameter is integer
- *
- * @b: parameter is integer
- *
- * Return: returns the difference of a and b
-*/
-
-int op_sub(int a, int b)
-{
-return (a - b);
-}
-
-/**
- * op_mul - function that product of a and b
- *
- * @a: parameter is integer
- *
- * @b: parameter is integer
- *
- * Return:  returns the product of a and b
-*/
-
-int op_mul(int a, int b)
-{
-return (a * b);
-}
-
-/**
- * op_div - function that division of a by b
- *
- * @a: parameter is integer
- *
- * @b: parameter is integer
- *
- * Return: returns the result of the division of a by b
-*/
-
-int op_div(int a, int b)
-{
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-return (a / b);
-}
-
-/**
- * op_mod - function that return remainder of the division
- *
- * @a: parameter is integer
- *
- * @b: parameter is integer
- *
- * Return: returns the remainder of the division of a by b
-*/
-
-int op_mod(int a, int b)
-{
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-return (a % b);
-}
 
